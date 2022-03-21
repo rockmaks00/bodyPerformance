926b00b3fcc4788dcbe0b7ab1bd958c9ed8343ee
Gashuk <66191970+Gashuk@users.noreply.github.com>
2022-03-22 01:06:51 +0500

    Добавил личный кабинет с таблицей и графиком

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 import json
+from django.core import serializers
 
 from django.shortcuts import render, redirect
 from .models import Body
@@ -45,4 +46,4 @@ def result(request):
 @login_required(login_url='/login/')
 def profile(request):
     data = Body.objects.filter(author=request.user.id)
-    return render(request, 'profile.html', {'data': data, 'data_json': json.dumps(data)})
+    return render(request, 'profile.html', {'data': data, 'data_json': serializers.serialize("json", data)})
